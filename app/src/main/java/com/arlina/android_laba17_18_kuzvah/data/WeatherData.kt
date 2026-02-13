1eb3f94daf853e6b5e0fd0c302f223d5e26b44e9
Artur <only.up.ak@gmail.com>
2026-02-13 21:14:24 +0300

    Step 5: Create ViewModel with sequential loading

--- app/src/main/java/com/arlina/android_laba17_18_kuzvah/data/WeatherData.kt
+++ app/src/main/java/com/arlina/android_laba17_18_kuzvah/data/WeatherData.kt
@@ -2,7 +2,7 @@ package com.arlina.android_laba17_18_kuzvah.data
 
 data class WeatherData(
     val temperature: Int? = null,
-    val humidiry: Int? = null,
+    val humidity: Int? = null,
     val windSpeed: Int? = null,
     val isLoading: Boolean = false,
     var error: String? = null
